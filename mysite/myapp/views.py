ffff994a81ad2d8d3d460dc5d06a576f222123b8
acilveti <cilveti.ander.92@gmail.com>
2016-08-29 08:15:28 +0000

    added NEWPAGE words for the new pages words data update

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -116,7 +116,19 @@ class wordajax(APIView):
         click_user = User.objects.get(pk=uid)
         print("the user is")
         print(click_user)
-        word_data = WordsUse(user = click_user, english_text = words, translation_active = True, aparitions = 0, click = 0) #increment of clicked, and switch translatio_active on ((user=click_user, english_text=words, translation_active = True,
+        word_data = WordsUse.objects.filter(user = click_user, english_text = words)
+        if len(word_data) is 1:
+            print("the get has returned the next object")
+            print(word_data[0])
+            word_data=word_data[0]
+            word_data.translation_active = True
+            word_data.click += 1
+        else:
+            if len(word_data) is 0:
+                word_data = WordsUse(user = click_user, english_text = words, translation_active = True, aparitions = 0, click = 1) #increment of clicked, and switch translatio_active on ((user=click_user, english_text=words, translation_active = True,
+            else:
+                print(" ERROR:there are more than one DB objects." + len(word_data))
+
 
         #pruebaword = WordsUse.objects.get(aparitions=0)
 
@@ -149,4 +161,52 @@ class wordajax(APIView):
         #print("fin")
 
         return Response(serializer.data)
+
+def newpagewords(request):
+    test = "I want"
+    test=test.lower()
+    words = test.split()
+
+    print(words)
+    for i in range(0,len(words)):
+        print(words[i])
+
+
+    session_key = request.session._session_key
+    print("the session key is")
+    print(request.session._session_key)
+
+    session = Session.objects.get(session_key=session_key)
+    uid = session.get_decoded().get('_auth_user_id')
+    click_user = User.objects.get(pk=uid)
+    print("the user is")
+    print(click_user)
+
+    for i in range(0,len(words)):
+
+        words_obj = Word.objects.get(english_text = words[i])
+        word_data = WordsUse.objects.filter(user = click_user, english_text = words_obj)
+
+        if len(word_data) is 1:
+            word_data_obj=word_data[0]
+            print("the get has returned the next object")
+            print(word_data_obj)
+            word_data_obj.aparitions += 1
+            word_data_obj.save()
+        else:
+            if len(word_data) is 0:
+                print(" there was not word in DB")
+                word_data = WordsUse(user = click_user, english_text = words, translation_active = True, aparitions = 1, click = 0) #increment of clicked, and switch translatio_active on ((user=click_user, english_text=words, translation_active = True,
+                word_data.save()
+            else:
+                print(" ERROR:there are more than one DB objects." + len(word_data))
+
+        # when certain number of aparitions are done, the translation should be switched off. task to do in the future
+
+
+
+    print(words)
+    return HttpResponse("I want")
+
+
 
